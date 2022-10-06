6d3ec682f83d9c2105f11243c375d19b305a44b6
Shananiganz <samantha.shanahan@georgebrown.ca>
2022-10-06 15:07:03 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public Boundary boundary;
+    public float verticalPosition;
+    public bool usingMobileInput = false;
+    public ScoreManager scoreManager;
+
+    private Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreManager = FindObjectOfType<ScoreManager>();
+
+        transform.position = new Vector2(0.0f, verticalPosition);
+        camera = Camera.main;
+
+        // Platform Detection for input
+        usingMobileInput = Application.platform == RuntimePlatform.Android ||
+                           Application.platform == RuntimePlatform.IPhonePlayer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (usingMobileInput)
+        {
+            GetMobileInput();
+        }
+        else
+        {
+            GetConventionalInput();
+        }
+
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            scoreManager.AddPoints(10);
+        }
+    }
+
+    void GetConventionalInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        transform.position += new Vector3(x, 0, 0);
+    }
+
+    void GetMobileInput()
+    {
+
+        foreach (Touch touch in Input.touches)
+        {
+            var destination = camera.ScreenToWorldPoint(touch.position);
+            transform.position = Vector2.Lerp(transform.position, destination, Time.deltaTime * speed);
+        }
+    }
+
+    void Move()
+    {
+        float clampedXPosition = Mathf.Clamp(transform.position.x, boundary.min, boundary.max);
+        transform.position = new Vector2(clampedXPosition, verticalPosition);
+    }
+
+}
