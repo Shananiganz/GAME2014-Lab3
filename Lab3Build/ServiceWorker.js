c70ad471dd51ebf46a2195548cdbe78c829047c3
Shananiganz <samantha.shanahan@georgebrown.ca>
2022-10-09 12:54:47 -0400

    Added Lab3 Build

---
+++ Lab3Build/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "GAME2014_ShanahanSamantha-GAME2014-Lab3-1.0.5";
+const contentToCache = [
+    "Build/Lab3Build.loader.js",
+    "Build/Lab3Build.framework.js.unityweb",
+    "Build/Lab3Build.data.unityweb",
+    "Build/Lab3Build.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
